fce2032a46b52d6195b195e7dc49ff3ff3a1ec61
Apil Kakkar <43724844+apil1056@users.noreply.github.com>
2021-02-03 17:53:40 +0530

    movement start

--- index.js
+++ index.js
@@ -7,8 +7,22 @@ const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
 
 // clearing screen bw every frame
-ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
 
 let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
 
 paddle.draw(ctx);
+
+// GAME LOOP
+let lastTime = 0;
+
+function gameLoop(timestamp) {
+  let deltaTime = timestamp - lastTime;
+  lastTime = timestamp;
+  ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+  paddle.update(deltaTime);
+  paddle.draw(ctx);
+
+  requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
