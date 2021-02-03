84b6dbaa73453c09a9306971b3e9855b4c445c11
Apil Kakkar <43724844+apil1056@users.noreply.github.com>
2021-02-03 19:44:33 +0530

    collision with paddle

--- ball.js
+++ ball.js
@@ -1,13 +1,15 @@
 export default class Ball {
   constructor(game) {
+    this.image = document.getElementById("img_ball");
+
     this.gameWidth = game.gameWidth;
     this.gameHeight = game.gameHeight;
-    this.image = document.getElementById("img_ball");
+
+    this.game = game;
+
     this.position = { x: 10, y: 10 };
-    this.speed = {
-      x: 2,
-      y: 2
-    };
+    this.speed = { x: 2, y: 2 };
+
     this.size = 20;
   }
 
@@ -26,11 +28,29 @@ export default class Ball {
     this.position.x += this.speed.x;
     this.position.y += this.speed.y;
 
+    // wall on left or right
     if (this.position.x + this.size > this.gameWidth || this.position.x < 0) {
       this.speed.x = -this.speed.x;
     }
+
+    // wall on top or bottom
     if (this.position.y + this.size > this.gameHeight || this.position.y < 0) {
       this.speed.y = -this.speed.y;
+    }
+
+    //check collision with paddle
+    let bottomOfBall = this.position.y + this.size;
+    let topOfPaddle = this.game.paddle.position.y;
+    let leftOfPaddle = this.game.paddle.position.x;
+    let rightOfPaddle = this.game.paddle.position.x + this.game.paddle.width;
+
+    if (
+      bottomOfBall >= topOfPaddle &&
+      this.position.x >= leftOfPaddle &&
+      this.position.x + this.size <= rightOfPaddle
+    ) {
+      this.speed.y = -this.speed.y;
+      this.position.y = this.game.paddle.position.y - this.size;
     }
   }
 }
