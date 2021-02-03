9193556e96396c1e1f61f8296ae5b0fced5b4a43
Apil Kakkar <43724844+apil1056@users.noreply.github.com>
2021-02-03 18:40:54 +0530

    ball in canvas

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import Paddle from "./paddle.js";
 import InputHandler from "./input.js";
+import Ball from "./ball.js";
 
 let canvas = document.getElementById("gameScreen");
 let ctx = canvas.getContext("2d");
@@ -10,11 +11,14 @@ const GAME_HEIGHT = 600;
 // clearing screen bw every frame
 
 let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
+let ball = new Ball();
 
 new InputHandler(paddle);
 
 // GAME LOOP
 let lastTime = 0;
+
+//images
 
 function gameLoop(timestamp) {
   let deltaTime = timestamp - lastTime;
@@ -22,6 +26,8 @@ function gameLoop(timestamp) {
   ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
   paddle.update(deltaTime);
   paddle.draw(ctx);
+
+  ball.draw(ctx);
 
   requestAnimationFrame(gameLoop);
 }
