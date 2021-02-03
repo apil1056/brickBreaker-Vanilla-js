dbe7c4b0d89356dc0ed4eecbe80d6152a413b991
Apil Kakkar <43724844+apil1056@users.noreply.github.com>
2021-02-04 01:47:31 +0530

    trying pause

--- game.js
+++ game.js
@@ -5,6 +5,13 @@ import Brick from "./brick.js";
 
 import { buildLevel, level1 } from "./levels.js";
 
+const GAMESTATE = {
+  PAUSED: 0,
+  RUNNING: 1,
+  MENU: 2,
+  GAMEOVER: 3
+};
+
 export default class Game {
   constructor(gameWidth, gameHeight) {
     this.gameWidth = gameWidth;
@@ -12,16 +19,18 @@ export default class Game {
   }
 
   start() {
+    this.gamestate = GAMESTATE.RUNNING;
     this.paddle = new Paddle(this);
     this.ball = new Ball(this);
     //let bricks = buildLevel(this, level1);
     let bricks = buildLevel(this, level1);
     this.gameObjects = [this.ball, this.paddle, ...bricks];
 
-    new InputHandler(this.paddle);
+    new InputHandler(this.paddle, this);
   }
 
   update(deltaTime) {
+    if (this.gamestate == GAMESTATE.PAUSED) return;
     this.gameObjects.forEach((object) => object.update(deltaTime));
 
     this.gameObjects = this.gameObjects.filter(
@@ -31,5 +40,13 @@ export default class Game {
 
   draw(ctx) {
     this.gameObjects.forEach((object) => object.draw(ctx));
+  }
+
+  togglePause() {
+    if (this.gamestate == GAMESTATE.PAUSED) {
+      this.gamestate = GAMESTATE.RUNNING;
+    } else {
+      this.GAMESTATE = GAMESTATE.PAUSED;
+    }
   }
 }
