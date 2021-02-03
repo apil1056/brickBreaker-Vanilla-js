888072a2cb09170a0c8ed360fa3b15b1ed0cff69
Apil Kakkar <43724844+apil1056@users.noreply.github.com>
2021-02-04 01:19:01 +0530

    collision detection

--- ball.js
+++ ball.js
@@ -1,3 +1,5 @@
+import { detectCollision } from "./collisionDetection.js";
+
 export default class Ball {
   constructor(game) {
     this.image = document.getElementById("img_ball");
@@ -39,16 +41,7 @@ export default class Ball {
     }
 
     //check collision with paddle
-    let bottomOfBall = this.position.y + this.size;
-    let topOfPaddle = this.game.paddle.position.y;
-    let leftOfPaddle = this.game.paddle.position.x;
-    let rightOfPaddle = this.game.paddle.position.x + this.game.paddle.width;
-
-    if (
-      bottomOfBall >= topOfPaddle &&
-      this.position.x >= leftOfPaddle &&
-      this.position.x + this.size <= rightOfPaddle
-    ) {
+    if (detectCollision(this, this.game.paddle)) {
       this.speed.y = -this.speed.y;
       this.position.y = this.game.paddle.position.y - this.size;
     }
